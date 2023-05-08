e4c8c4a5a160e4859eeecd0ec3640a60f27b3e18
= <=>
2023-05-08 18:26:09 +0600

    Add StudentRepsitory methods

--- src/main/java/com/springdata/jpa/repositories/StudentRepository.java
+++ src/main/java/com/springdata/jpa/repositories/StudentRepository.java
@@ -1,10 +1,22 @@
 package com.springdata.jpa.repositories;
 
 import com.springdata.jpa.entities.Student;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Optional<Student> findByFirstName(String firstName);
+
+    Optional<Student> findByAge(int age);
+
+    boolean existByEmail(String email);
+
+    List<Student> findAllByAgeGreaterThanEqual(int age);
 
 }
