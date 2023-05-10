bb104848e2ad9def96c18d11b4bba5d60cdf2cfe
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-10 10:34:02 +0600

    Add AppTables.java

---
+++ src/main/java/com/springdata/jpa/constants/AppTables.java
@@ -0,0 +1,25 @@
+package com.springdata.jpa.constants;
+
+public final class AppTables {
+
+    private AppTables(){}
+
+    public static final String STUDENT = "STUDENT";
+
+    public static final class StudentTable {
+
+        private StudentTable(){}
+
+        public static final String ID = "ID";
+
+        public static final String FIRST_NAME = "FIRST_NAME";
+
+        public static final String LAST_NAME = "LAST_NAME";
+
+        public static final String EMAIL = "EMAIL";
+
+        public static final String AGE = "AGE";
+
+    }
+
+}
