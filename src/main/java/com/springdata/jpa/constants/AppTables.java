147c832e2d4ef34cb5a6255bedf7c78196be5cd9
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-10 10:47:23 +0600

    Modify AppTables.java

--- src/main/java/com/springdata/jpa/constants/AppTables.java
+++ src/main/java/com/springdata/jpa/constants/AppTables.java
@@ -6,11 +6,25 @@ public final class AppTables {
 
     public static final String STUDENT = "STUDENT";
 
+    public static final class AuditModelTable {
+
+        private AuditModelTable(){}
+
+        public static final String ID = "ID";
+
+        public static final String CREATED_BY = "CREATED_BY";
+
+        public static final String CREATION_DATE = "CREATION_DATE";
+
+        public static final String LAST_MODIFIED_BY = "LAST_MODIFIED_BY";
+
+        public static final String LAST_MODIFICATION_DATE = "LAST_MODIFICATION_DATE";
+
+    }
+
     public static final class StudentTable {
 
         private StudentTable(){}
-
-        public static final String ID = "ID";
 
         public static final String FIRST_NAME = "FIRST_NAME";
 
