4f6a417987647ba56824018feda02bf71c8030f5
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-10 13:12:19 +0600

    Modify Student.java entity

--- src/main/java/com/springdata/jpa/entities/Student.java
+++ src/main/java/com/springdata/jpa/entities/Student.java
@@ -1,81 +1,35 @@
 package com.springdata.jpa.entities;
 
+import com.springdata.jpa.constants.AppTables;
+import com.springdata.jpa.constants.AppTables.StudentTable;
+import com.springdata.jpa.models.AuditModel;
 import jakarta.persistence.*;
 
 @Entity
-@Table(
-        name = "STUDENT",
-        uniqueConstraints = {
-                @UniqueConstraint(
-                        name = "student_email_unique",
-                        columnNames = "EMAIL"
-                )
-        }
-)
-public class Student {
+@Table(name = AppTables.STUDENT)
+public class Student extends AuditModel<String> {
 
-    @Id
-    @SequenceGenerator(
-            name = "student_seq",
-            sequenceName = "STUDENT_SEQUENCE",
-            allocationSize = 1
-    )
-    @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "student_seq"
-    )
-    @Column(
-            name = "ID",
-            updatable = false
-    )
-    private long id;
-
-    @Column(
-            name = "FIRST_NAME",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
+    @Column(name = StudentTable.FIRST_NAME)
     private String firstName;
 
-    @Column(
-            name = "LAST_NAME",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
+    @Column(name = StudentTable.LAST_NAME)
     private String lastName;
 
-    @Column(
-            name = "EMAIL",
-            nullable = false,
-            columnDefinition = "TEXT"
-    )
+    @Column(name = StudentTable.EMAIL, nullable = false, unique = true)
     private String email;
 
-    @Column(
-            name = "AGE",
-            nullable = false,
-            columnDefinition = "INTEGER"
-    )
+    @Column(name = StudentTable.AGE)
     private int age;
 
     public Student() {
 
     }
 
-    public Student(long id, String firstName, String lastName, String email, int age) {
-        this.id = id;
+    public Student(String firstName, String lastName, String email, int age) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.age = age;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
     }
 
     public String getFirstName() {
