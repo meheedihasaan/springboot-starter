c98ecf7ec842cad1a46f9acdba40e628218aa4bf
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-10 15:22:22 +0600

    Modify AppTables.java

--- src/main/java/com/springdata/jpa/constants/AppTables.java
+++ src/main/java/com/springdata/jpa/constants/AppTables.java
@@ -8,7 +8,7 @@ public final class AppTables {
 
     public static final class AuditModelTable {
 
-        private AuditModelTable(){}
+        private AuditModelTable() {}
 
         public static final String ID = "ID";
 
@@ -22,9 +22,57 @@ public final class AppTables {
 
     }
 
+    public static final class UserTable {
+
+        private UserTable() {}
+
+        private static final String USER_ID = "USER_ID";
+
+        private static final String EMAIL = "EMAIL";
+
+        private static final String PASSWORD = "PASSWORD";
+
+        private static final String PASSWORD_REFRESH_TOKEN = "PASSWORD_REFRESH_TOKEN";
+
+        private static final String NAME = "NAME";
+
+        private static final String VERIFIED = "VERIFIED";
+
+        private static final String BANNED = "BANNED";
+
+    }
+
+    public static final class RoleTable {
+
+        private RoleTable() {}
+
+        private static final String ROLE_ID = "ROLE_ID";
+
+        private static final String ROLE_TYPE = "ROLE_TYPE";
+
+        private static final String ROLE_NAME = "ROLE_NAME";
+
+        private static final String DESCRIPTION = "DESCRIPTION";
+
+        private static final String IMAGE_URL = "IMAGE_URL";
+
+    }
+
+    public static final class PrivilegeTable {
+
+        private PrivilegeTable() {}
+
+        public static final String PRIVILEGE_ID = "PRIVILEGE_ID";
+
+        public static final String PRIVILEGE_NAME = "PRIVILEGE_NAME";
+
+        public static final String DESC_NAME = "DESC_NAME";
+
+    }
+
     public static final class StudentTable {
 
-        private StudentTable(){}
+        private StudentTable() {}
 
         public static final String FIRST_NAME = "FIRST_NAME";
 
