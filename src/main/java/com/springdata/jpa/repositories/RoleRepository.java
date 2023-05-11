a007c8cfbf389e8e5f8f01e03b44149f762516dd
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-11 10:01:20 +0600

    Add RoleRepository.java

---
+++ src/main/java/com/springdata/jpa/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.springdata.jpa.repositories;
+
+import com.springdata.jpa.entities.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
+
+}
