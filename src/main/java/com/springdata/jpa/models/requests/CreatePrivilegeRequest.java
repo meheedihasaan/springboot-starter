6ffdca665559de4826ef68092968893afb17fa48
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-11 10:38:44 +0600

    Add CreatePrivilegeRequest.java

---
+++ src/main/java/com/springdata/jpa/models/requests/CreatePrivilegeRequest.java
@@ -0,0 +1,25 @@
+package com.springdata.jpa.models.requests;
+
+public class CreatePrivilegeRequest {
+
+    private String privilegeName;
+
+    private String description;
+
+    public String getPrivilegeName() {
+        return privilegeName;
+    }
+
+    public void setPrivilegeName(String privilegeName) {
+        this.privilegeName = privilegeName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
