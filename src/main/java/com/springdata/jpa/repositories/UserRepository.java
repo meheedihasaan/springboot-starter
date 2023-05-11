0e2cb53745ea5cb24146a2a96ca61913b4729285
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-11 09:58:30 +0600

    Add UserRepository.java

---
+++ src/main/java/com/springdata/jpa/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.springdata.jpa.repositories;
+
+import com.springdata.jpa.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
+
+}
