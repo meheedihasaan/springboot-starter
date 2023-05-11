1e75069d38300ca66cd86af0fb3054ff3d9af5c7
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-11 10:22:32 +0600

    Add UserService.java

---
+++ src/main/java/com/springdata/jpa/services/UserService.java
@@ -0,0 +1,17 @@
+package com.springdata.jpa.services;
+
+import com.springdata.jpa.entities.User;
+
+public interface UserService {
+
+    public void saveUser();
+
+    public User findById(Long id);
+
+    public User findByIdWithException(Long id);
+
+    public User findByEmail(String email);
+
+    public User findByEmailWithException(String email);
+
+}
