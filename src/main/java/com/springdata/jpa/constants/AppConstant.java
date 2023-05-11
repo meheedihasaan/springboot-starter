3c51129a51114c2989fe250b45470c1ed0ec6e1e
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-11 12:12:42 +0600

    Modify AppConstant.java

--- src/main/java/com/springdata/jpa/constants/AppConstant.java
+++ src/main/java/com/springdata/jpa/constants/AppConstant.java
@@ -1,6 +1,11 @@
 package com.springdata.jpa.constants;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public final class AppConstant {
+
+    private AppConstant() {}
 
     public static final String PAGE_NUMBER = "pageNumber";
 
@@ -11,5 +16,37 @@ public final class AppConstant {
     public static final String ASC_OR_DESC_TYPE = "ascOrDescType";
 
     public static final String PARAMETERS = "parameters";
+
+    public static final String INITIAL_USERNAME = "system@gmail.com";
+
+    public static final String INITIAL_PASSWORD = "system";
+
+    public static final String INITIAL_ROLE = "ADMIN";
+
+    public static final String USER_ROLE = "USER";
+
+    public static final String CONSUMER_PERMISSION = "USER";
+
+    public static final String CONSUMER_PERMISSION_DESCRIPTION = "User Generalized Permission";
+
+    public static final Map<String, String> PERMISSIONS = new HashMap<>() {
+        {
+            put("GENERAL", "GENERAL CONSUMER");
+
+            put("USER_CREATE", "USER CREATE");
+            put("USER_READ", "USER READ");
+            put("USER_UPDATE", "USER UPDATE");
+            put("USER_DELETE", "USER DELETE");
+
+            put("ROLE_CREATE", "ROLE CREATE");
+            put("ROLE_READ", "ROLE DELETE");
+            put("ROLE_UPDATE", "ROLE UPDATE");
+            put("ROLE_DELETE", "ROLE DELETE");
+
+            put("USER_UPDATE_FROM_ADMIN", "USER UPDATE FROM ADMIN");
+        }
+    };
+
+
 
 }
