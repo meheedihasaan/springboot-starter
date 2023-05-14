8bef22c5d05885a31f6ef232168aed33772b7780
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-14 12:13:47 +0600

    Add ResponseException.java

---
+++ src/main/java/com/springboot/starter/exceptions/ResponseException.java
@@ -0,0 +1,37 @@
+package com.springboot.starter.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class ResponseException extends RuntimeException {
+
+    private HttpStatus httpStatus;
+
+    private Object payload;
+
+    public ResponseException(String message) {
+        super(message);
+    }
+
+    public ResponseException(HttpStatus httpStatus, String message, Object payload) {
+        super(message);
+        this.httpStatus = httpStatus;
+        this.payload = payload;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+    public void setHttpStatus(HttpStatus httpStatus) {
+        this.httpStatus = httpStatus;
+    }
+
+    public Object getPayload() {
+        return payload;
+    }
+
+    public void setPayload(Object payload) {
+        this.payload = payload;
+    }
+
+}
