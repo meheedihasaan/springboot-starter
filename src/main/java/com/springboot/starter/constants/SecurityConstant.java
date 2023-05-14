168b29ea0f6c3879c08c75a76ca147756855c5c7
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-14 18:47:59 +0600

    Modify SecurityConstant.java and SecurityConfig.java

--- src/main/java/com/springboot/starter/constants/SecurityConstant.java
+++ src/main/java/com/springboot/starter/constants/SecurityConstant.java
@@ -15,4 +15,28 @@ public final class SecurityConstant {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final long REFRESH_TOKEN_EXPIRATION_TIME =  TimeUnit.DAYS.toMillis(10);
 
+    public static final String[] JWT_DISABLE_ANTMATCHERS = {
+            "/swagger-ui.html",
+            "/resetpassword/**",
+            "/pass-reset",
+            "/api/signin",
+            "/api/signup",
+            "/api/forgetpassword",
+            "/api-documentation.html",
+            "/swagger-ui/**",
+            "/api-docs/**",
+            "/api/verify/**",
+            "/v3/api-docs/**",
+            "/configuration/ui",
+            "/swagger-resources/**",
+            "/configuration/security",
+            "/webjars/**",
+            "/verify*",
+            "/api/oauth2/**",
+            "/api/refreshtoken",
+            "/api/db-file/id/**",
+            "/api/page/tag/**",
+            "/api/page/predefine-tags",
+    };
+
 }
--- src/main/java/com/springboot/starter/security/SecurityConfig.java
+++ src/main/java/com/springboot/starter/security/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.springboot.starter.security;
 
+import com.springboot.starter.constants.SecurityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -20,14 +25,21 @@ public class SecurityConfig {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf().disable().cors().disable()
-                .authorizeHttpRequests()
-                .anyRequest().authenticated().and().httpBasic();
-
-        httpSecurity.authenticationProvider(authenticationProvider());
+                .securityMatcher("/api/**").authorizeHttpRequests()
+                .requestMatchers(SecurityConstant.JWT_DISABLE_ANTMATCHERS).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .requiresChannel().requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null).requiresSecure();
 
         return httpSecurity.build();
     }
@@ -38,6 +50,11 @@ public class SecurityConfig {
         daoAuthenticationProvider.setUserDetailsService(userDetailsService);
         daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
         return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
 }
