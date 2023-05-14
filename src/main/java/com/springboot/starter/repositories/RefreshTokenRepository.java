3cbc8fa8eda36ff522a26ea5c3a43dde3ecda5ea
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-14 11:12:11 +0600

    Add RefreshToken

---
+++ src/main/java/com/springboot/starter/repositories/RefreshTokenRepository.java
@@ -0,0 +1,22 @@
+package com.springboot.starter.repositories;
+
+import com.springboot.starter.entities.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+
+    Optional<RefreshToken> findById(Long id);
+
+    Optional<RefreshToken> findByToken(String token);
+
+    List<RefreshToken> findByUserIdAndExpiryDateIsBefore(Long userId,  Instant currentTime);
+
+
+
+}
