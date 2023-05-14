25e535e3faa0aeca557593bc233c00845e2838db
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-14 19:36:56 +0600

    Add AuthenticationExceptionHandler.java

---
+++ src/main/java/com/springboot/starter/exceptions/AuthenticationExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.springboot.starter.exceptions;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.starter.models.Response;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+public class AuthenticationExceptionHandler implements AuthenticationEntryPoint, Serializable {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        ObjectMapper mapper = new ObjectMapper();
+        Response errorResponse = new Response(HttpStatus.FORBIDDEN, "(JWT) " + authException.getMessage(), null, false);
+        String responseMessage = mapper.writeValueAsString(errorResponse);
+        response.getWriter().write(responseMessage);
+        response.setStatus(403);
+    }
+
+}
