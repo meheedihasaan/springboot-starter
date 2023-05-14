a69c4f8d7d35eb6dec52b159862ff04a5399ce89
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-14 10:59:49 +0600

    Add RefreshToken.java entity

---
+++ src/main/java/com/springboot/starter/entities/RefreshToken.java
@@ -0,0 +1,49 @@
+package com.springboot.starter.entities;
+
+import com.springboot.starter.constants.AppTables;
+import com.springboot.starter.constants.AppTables.RefreshTokenTable;
+import com.springboot.starter.models.AuditModel;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = AppTables.REFRESH_TOKEN_TABLE)
+public class RefreshToken extends AuditModel<String> {
+
+    @Column(name = RefreshTokenTable.USER_ID)
+    private Long userId;
+
+    @Column(name = RefreshTokenTable.TOKEN)
+    private String token;
+
+    @Column(name = RefreshTokenTable.EXPIRY_DATE, nullable = false)
+    private Instant expiryData;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Instant getExpiryData() {
+        return expiryData;
+    }
+
+    public void setExpiryData(Instant expiryData) {
+        this.expiryData = expiryData;
+    }
+
+}
