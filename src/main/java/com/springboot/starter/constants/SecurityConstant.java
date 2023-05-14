def7553bd02c0a232e9a46f21ff057fa9f2b924f
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-14 14:48:05 +0600

    Modify SecurityConstant.java

--- src/main/java/com/springboot/starter/constants/SecurityConstant.java
+++ src/main/java/com/springboot/starter/constants/SecurityConstant.java
@@ -1,11 +1,15 @@
 package com.springboot.starter.constants;
 
+import io.jsonwebtoken.SignatureAlgorithm;
+
 import java.util.concurrent.TimeUnit;
 
 public final class SecurityConstant {
 
     private SecurityConstant() {}
 
+    public static final long JWT_TOKEN_EXPIRATION_TIME = TimeUnit.DAYS.toMillis(3);
+    public static final SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS512;
     public static final long REFRESH_TOKEN_EXPIRATION_TIME =  TimeUnit.DAYS.toMillis(10);
 
 }
