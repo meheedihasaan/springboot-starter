2fe4e01b70c543a1f1090cee4c0cbc1bfeeaa48d
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-15 11:55:45 +0600

    Add UserRepository.java

--- src/main/java/com/springboot/starter/repositories/UserRepository.java
+++ src/main/java/com/springboot/starter/repositories/UserRepository.java
@@ -12,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificat
 
     Optional<User> findByEmail(String email);
 
+    Boolean existsUserByEmail(String email);
+
 }
