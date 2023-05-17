ad63b88c17ab1d6ee68144d2e9ff73a22f8945ed
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-17 12:03:42 +0600

    Add ChangePasswordRequest.java

---
+++ src/main/java/com/springboot/starter/models/requests/ChangePasswordRequest.java
@@ -0,0 +1,24 @@
+package com.springboot.starter.models.requests;
+
+public class ChangePasswordRequest {
+
+    private String oldPassword;
+
+    private String newPassword;
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
