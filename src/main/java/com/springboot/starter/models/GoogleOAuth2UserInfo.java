2ce61500a344cafc61af4350a608eb627bd6936a
meheedihasaan <mehedihasan111941@gmail.com>
2023-05-18 17:12:35 +0600

    Agg GoggleOAuth2UserInfo.java

---
+++ src/main/java/com/springboot/starter/models/GoogleOAuth2UserInfo.java
@@ -0,0 +1,37 @@
+package com.springboot.starter.models;
+
+import java.util.Map;
+
+public class GoogleOAuth2UserInfo extends OAuth2UserInfo{
+
+    private Map<String, Object> attributes;
+
+    public GoogleOAuth2UserInfo(Map<String, Object> attributes) {
+        super(attributes);
+    }
+
+    @Override
+    public String getId() {
+        return (String) attributes.get("sub")
+    }
+
+    @Override
+    public String getFistName() {
+        return (String) attributes.get("given_name");
+    }
+
+    @Override
+    public String getLastName() {
+        return (String) attributes.get("family_name");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+
+    @Override
+    public String getImageUrl() {
+        return (String) attributes.get("picture");
+    }
+}
